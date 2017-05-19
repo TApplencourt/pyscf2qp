d5728ba8b9c933db1f1ecd717ab0378186aa25c4
TApplencourt <tapplencourt@jlselogin2.ftm.alcf.anl.gov>
2017-05-19 21:47:29 +0000

    Implement 4 idx transformation

--- pyscf_fcidump.py
+++ pyscf_fcidump.py
@@ -19,13 +19,10 @@ int_threshold = 1E-8 # The integral will be not printed in they are bellow that
 
 # Cell creation
 # TODO: What is the difference between gto.M and gto.Cell ?!
-L=1
+L=3.5
 cell = gto.M(
-    atom = '''He 0. 0. 0.
-	      He 0.45 0. 0.
-	      He 0. 0.5 0.
-	      He 0.5 0.5 0.''',
-    basis = 'sto-3g',
+    atom = '''He 0. 0. 0.''',
+    basis = '6-31g',
     a =  numpy.diag([L,L,L]), # Cell dimension
     gs = [10]*3,
     verbose= 2)
@@ -41,10 +38,16 @@ assert (cell.spin == 0) # cell.spin == 2*S aka  multiplicity-1.
 
 # We handle only gamma point for now.
 # WARNING: But pyscf have a strange behaviour in ERI for  gama point (The dimmesion is reduced by (1) )
-# 	   So we shift the gamma point.
+# 	   So please shift the gamma point by epsilon.
 
-kpts_abs = [ [1E-5, 0., 0.] ]
-kpts = numpy.array([cell.get_abs_kpts(k) for k in kpts_abs])
+kpts_rel     = [ [0.5, 0.5,  0.] ]
+kpts_rel_inv = [ [-1*i for i in k] for k in kpts_rel ]
+
+kpts_abs    = [cell.get_abs_kpts(k) for k in kpts_rel]
+kpts_abs_inv= [cell.get_abs_kpts(k) for k in kpts_rel_inv]
+
+kpts = numpy.array(kpts_abs+kpts_abs_inv)
+
 # More the general but with the bug
 #nk = [1,1,1] # Number of Kpoint in each direction
 #kpts = cell.make_kpts(nk) #kpts in absolute value (unit 1/Bohr).
@@ -68,17 +71,16 @@ use_density_fiting = True
 if use_density_fiting:
 	mydf = df.MDF(cell) # MDF seem beter than DF. No idea why. 
 	mydf.kpts = kpts
-	mydf.auxbasis = 'weigend' # Note: No idea what is this. But the use is all the time
+	mydf.auxbasis = 'weigend' # Note: No idea what 'weigend' is this. But the use it all the time in the example.
 	kmf.with_df = mydf
 
 # Do thr SCF and the MO. 
 e_scf = kmf.kernel()
 print 'e_scf',e_scf
-
 print ''
+
 mo_coeff = kmf.mo_coeff # List of mo_coeff for each k-point
 nmo = mo_coeff.shape[1]
-
 print 'mo_tot_num',nmo
 
 # Wrote all the parameter need to creat a dummy EZFIO folder who will containt the integral after.
@@ -94,30 +96,67 @@ print 'nucl_repul', e_nuc
 with open('e_nuc','w') as f:
         f.write(str(e_nuc))
 
+
+# ~!
+# Now we will compute the needed integral
+# In Quantum Package we can only use REAL integral, so we will do some rotation
+# Note. We will rotate the integral and note the orbital. This is really inefficient!
+# ~!
+def get_kpt_idx(kpt,kpts):
+        return next(i for i,j in enumerate(kpts) if all( k==l for k,l in zip(j,kpt)))
+
 # ___                                              
 #  |  ._ _|_  _   _  ._ _. |  _   |\/|  _  ._   _  
 # _|_ | | |_ (/_ (_| | (_| | _>   |  | (_) | | (_) 
 #                 _|                              
+
+def mono_netherland_obliteration(kpt,kpt_inv,sigma,l_int):
+	#By symetrie only k1 | k2 is null if k1 != k2
+	#This is why we use kpt and do d_kpt 
+
+        def ao2mo(kpt):
+                idx_kpt = get_kpt_idx(kpt,kpts)
+                return reduce(numpy.dot, (mo_coeff[idx_kpt].T, l_int[idx_kpt], mo_coeff[idx_kpt]))
+
+        int_ = ao2mo(kpt)
+        int_inv = ao2mo(kpt_inv)
+
+	print int_
+	print int_inv
+	print ''
+        nu = 1 if sigma == 1 else 1j
+        l_int = (int_+ sigma*int_inv) / ( 2*nu)
+	# Need to check if this is real
+	
+	return numpy.real(l_int)
+
 from itertools import product
 
-def gen_mono_MO(mo_coeff,l_int,idx_kpt=0):
-	# 2Id transfortion Transformation. For now we handle only one K point.
-        l_int_mo = reduce(numpy.dot, (mo_coeff[idx_kpt].T, l_int[idx_kpt], mo_coeff[idx_kpt])) #This formula is only right for one kpt.
-	for i,j in product(range(nmo), repeat=2):
-                int_ = l_int_mo[i,j].real # Only to handle the gamma-point bug.
-		if abs(int_) > int_threshold:
-			yield (i+1,j+1, int_)
+def print_MO(l_int_mo,padding=0):
+     '''Print erything do not care about he symetrie'''
+     for i,j in product(range(nmo), repeat=2):
+            int_ = l_int_mo[i,j]
+            if abs(int_) > int_threshold:
+                    yield (padding+i+1,padding+j+1, int_)
 
 # A tuple with the name, and the value of the integrals for each k-pts
 t_ao = ('kinetic', cell.pbc_intor('cint1e_kin_sph',kpts=kpts)) #  Note .intor function computes integrals without PBC
 v_ao = ('nuclear', [kmf.with_df.get_nuc(kpt) for kpt in kpts]) # Ref:  `get_hcore` in pbc/scf file
 
-#Print
-for idx_kpt in range(nkpt): #Prepare the jon when we will have many k point
-	for name, ao in (t_ao,v_ao):
+
+for kpt, kpt_inv in zip(kpts_abs,kpts_abs_inv):
+
+	for name, l_int_ao in (t_ao,v_ao):	
+		l_mo = mono_netherland_obliteration(kpt,kpt_inv, 1,l_int_ao)
+	
 		with open('%s_mo' % name,'w') as f:
-			for mono in gen_mono_MO(mo_coeff,ao,idx_kpt):
-				f.write('%s %s %s\n'% mono)
+			l_mo = mono_netherland_obliteration(kpt,kpt_inv, 1,l_int_ao)
+                        for mono in print_MO(l_mo, padding=nmo*get_kpt_idx(kpt,kpts)):
+                                f.write('%s %s %s\n'% mono)
+
+	                l_mo = mono_netherland_obliteration(kpt,kpt_inv, -1,l_int_ao)
+                        for mono in print_MO(l_mo, padding=nmo*nkpt*get_kpt_idx(kpt,kpts)):
+                                f.write('%s %s %s\n'% mono)
 
 # ___                              _    
 #  |  ._ _|_  _   _  ._ _. |  _   |_) o 
@@ -125,42 +164,24 @@ for idx_kpt in range(nkpt): #Prepare the jon when we will have many k point
 #                 _|                    
 #
 
-def gen_eri_MO(kernel,kpts, qkpt_idx):
-	     
-	     # Compute the ERI
-	     eri_kpt_flatish = kernel.with_df.ao2mo([kernel.mo_coeff[i] for i in qkpt_idx],
-                                            	    [kpts[i] for i in qkpt_idx])
-
-	     nmo = kernel.mo_coeff.shape[1]
-	     # Putin in a good shape
-             eri_kpt = eri_kpt_flatish.reshape((nmo,)*4)
-	
-	     # Warning: We need to handle the 8-fold symetrie. If not this will bug in the QP size.
-	     # If you don't remenber it: http://vergil.chemistry.gatech.edu/notes/permsymm/permsymm.pdf	
-	     for i in range(nmo):
-		for j in range(0,i+1):
-			for k in range(0,i+1):
-				for l in range(0,k+1):
-			 		v =  eri_kpt[i,j,k,l].real #TODO THIS IS ONLY A HACK FOR THE GAMA POINT BUG (aka we need to a perturbation)
-								   # We need to do the cafaral perturbation one day
-                         		if (i*j >= k*l) and abs(v) > int_threshold:
-						yield (i+1,k+1,j+1,l+1,v)
-
-
-def get_kpt_idx(kpt,kpts):
-	return next(i for i,j in enumerate(kpts) if all( k==l for k,l in zip(j,kpt)))
-
 class Conveniant_notation(object):
 
 	def __init__(self, q_kpts,q_kpts_inv):
 		self.q_kpts = q_kpts
 		self.q_kpts_inv = q_kpts_inv
-	
+
+		self.cache = dict()	
 	def get_kpt(self,i):
 		array = self.q_kpts if i>0 else self.q_kpts_inv
 		return array[abs(i)-1]	
 
+
+
 	def get(self,t1,t2):
+		
+		if (t1,t2) in self.cache:
+			return self.cache[ (t1,t2) ]
+
 		k1, k3 = t1
 		k2, k4 = t2
 
@@ -168,28 +189,25 @@ class Conveniant_notation(object):
 		qkpt_idx = [get_kpt_idx(kpt, kpts) for kpt in q_kpts] 		
 
 
-		l_mo_coeff = [kernel.mo_coeff[i] for i in qkpt_idx]
+		l_mo_coeff = [kmf.mo_coeff[i] for i in qkpt_idx]
 	
                 # Compute the ERI
-                eri_kpt_flatish = kernel.with_df.ao2mo(l_mo_coeff,q_kpts)
+                eri_kpt_flatish = kmf.with_df.ao2mo(l_mo_coeff,q_kpts)
 
-      	        nmo = kernel.mo_coeff.shape[1]
+      	        nmo = kmf.mo_coeff.shape[1]
                 # Putin in a good shape
-                return eri_kpt_flatish.reshape((nmo,)*4)
-	
-		
-def netherland_obliteration(q_kpts,q_kpts_inv,q_sigma):
+		eri = eri_kpt_flatish.reshape((nmo,)*4)
+
+		self.cache[ (t1,t2) ] = eri
+                return eri
+
+def bi_netherland_obliteration(p,q_sigma):
 	#Chimist notation
 	#s = +1 or -1
-	print q_kpts
-	print q_kpts_inv
 
 	s1,s2,s3,s4 = q_sigma
-	p = Conveniant_notation(q_kpts,q_kpts_inv)
 
 	zero_sigma  =     p.get((1,2),(3,4))
-	print zero_sigma
-	print '--------'
 
 	one_sigma  =  s1*p.get((-1,2),(3,4)) + s2*p.get((1,-2),(3,4)) + \
 		      s3*p.get((1,2),(-3,4)) + s4*p.get((1,2),(3,-4))
@@ -204,21 +222,63 @@ def netherland_obliteration(q_kpts,q_kpts_inv,q_sigma):
 	four_sigma = s1*s2*s3*s4*p.get((-1,-2),(-3,-4))
  
 
-	l_nu = [1 if i == 1 else 1j for i in  [s1,s2,s3,s4]]
-	return (zero_sigma*two_sigma*three_sigma*four_sigma)/(16*numpy.prod(l_nu)) 
+	l_nu = [1 if i == 1 else 1j for i in  q_sigma]
+	return (zero_sigma+two_sigma+three_sigma+four_sigma)/(16*numpy.prod(l_nu)) 
 
 
-# Print all the eri
-# Note: Never tester with more than one K point.  
-kconserv = tools.get_kconserv(cell, kpts) # Get the momentum conservation array for a set of k-points.
+def gen_eri_MO(eri_kpt, q_padding):
+
+             # Warning: We need to handle the 8-fold symetrie. If not this will bug in the QP size.
+             # If you don't remenber it: http://vergil.chemistry.gatech.edu/notes/permsymm/permsymm.pdf
+             for i in range(nmo):
+                for j in range(0,i+1):
+                        for k in range(0,i+1):
+                                for l in range(0,k+1):
+                                        v =  eri_kpt[i,j,k,l] 
+                                        if (i*j >= k*l) and abs(v) > int_threshold:
+                                                yield (q_padding[0]+i+1,
+						       q_padding[2]+k+1,
+						       q_padding[1]+j+1,
+						       q_padding[3]+l+1,
+						       v)
+
+def get_padding(kpt,i):
+	return nmo*kpt if i == 1 else nmo*(nkpt/2+kpt)
+
+kconserv = tools.get_kconserv(cell, numpy.array(kpts_abs))
 with open('bielec_mo','w') as f:
-	for kp,kq,kr in product(range(nkpt),repeat=3): # Get all the kp,kq,kr possible
+	for kp,kq,kr in product(range(len(kpts_abs)),repeat=3): # Get all the kp,kq,kr possible
             ks = kconserv[kp,kq,kr] # Get the ks
-	    for eri in gen_eri_MO(kmf,kpts,(kp,kq,kr,ks)):
-                  f.write('%s %s %s %s %s\n' % eri)
-	    
 
-# TODO: CLEANING AND PRINTING
-#kernel = kmf
-#print netherland_obliteration([kpts[0]]*4,[kpts[1]]*4,[1,1,1,1])
 
+	    q_kpts = [kpts[kp],
+		      kpts[kq],
+		      kpts[kr],
+		      kpts[ks] ]
+	    q_kpts_inv = [ [-1*i for i in k] for k in q_kpts]
+
+
+	    print q_kpts
+	    print q_kpts_inv
+	    print ''
+	    engine = Conveniant_notation(q_kpts,q_kpts_inv)
+	    #Now do the permutation
+	    for p in [1, -1]:
+		p_pad = get_padding(kp,p)
+		for q in [1, -1]:
+			q_pad = get_padding(kq,q)
+			for r in [1, -1]:
+				r_pad = get_padding(kr,r)
+				for s in [1, -1]:
+					s_pad = get_padding(ks,s)
+
+					q_sigma = [p,q,r,s]
+					print q_sigma
+					print (p_pad,q_pad,r_pad,s_pad)
+					l_int = bi_netherland_obliteration(engine,q_sigma)
+					
+					for eri in gen_eri_MO(l_int,(p_pad,q_pad,r_pad,s_pad)):
+						 f.write('%s %s %s %s %s\n' % eri) 
+						 print eri
+				
+					print ''
